1bfe33bf1a9186e84aa73873c5f6a70a3be7c269
JuanManuelGasinghini <113570244+JuanManuelGasinghini@users.noreply.github.com>
2023-08-14 23:28:14 -0300

    formulario de contacto modificado

---
+++ contacto.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("contactForm");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      const nombre = form.nombre.value;
+      const email = form.email.value;
+      const provincia = form.provincia.value;
+      const ciudad = form.ciudad.value;
+      const mensaje = form.mensaje.value;
+  
+      const formData = {
+        nombre,
+        email,
+        provincia,
+        ciudad,
+        mensaje
+      };
+  
+      // Guardar los datos en el almacenamiento local (localStorage)
+      saveFormData(formData);
+  
+      // Limpiar el formulario
+      form.reset();
+    });
+  
+    function saveFormData(data) {
+      let formDataArray = JSON.parse(localStorage.getItem("formData")) || [];
+      formDataArray.push(data);
+      localStorage.setItem("formData", JSON.stringify(formDataArray));
+    }
+  });
+  
